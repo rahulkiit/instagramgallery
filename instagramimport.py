33fe3c746e345a73bd026fb615ba2213a5cfd694
rahul.ranjan@araido.com <rahul.ranjan@araido.com>
2021-08-07 13:49:46 +0200

    python import

---
+++ instagramimport.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import json
+
+access_token = os.environ.get('INSTAGRAM_ACCESS_TOKEN')
+response = requests.get("https://graph.instagram.com/me?access_token="+access_token)
+print(response.json())
+
+response = requests.get("https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type&access_token="+access_token)
+
+out_file = open("test.json", "w")
+#j = json.loads(response.json())
+out_file.write(json.dumps(response.json()['data'],indent=2))
